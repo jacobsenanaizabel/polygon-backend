15f51532a51380fe8fa4cc4184399661960121c8
Ana Izabel Jacobsen <13037895+jacobsenanaizabel@users.noreply.github.com>
2026-06-28 17:08:22 +0100

    feat: removing old workflow routes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import "reflect-metadata";
 import express from "express";
-import analysisRoutes from "./routes/analysisRoutes";
-import workflowRoutes from "./routes/workflowRoutes";
+import analysisRoutes from './routes/analysisRoutes';
 import defaultRoute from "./routes/defaultRoute";
 import { taskWorker } from "./workers/taskWorker";
 import { AppDataSource } from "./data-source"; // Import the DataSource instance
 
 const app = express();
 app.use(express.json());
-app.use("/analysis", analysisRoutes);
-app.use("/workflow", workflowRoutes);
+app.use('/analysis', analysisRoutes);
 app.use("/", defaultRoute);
 
 AppDataSource.initialize()
